0edd12dec1d6b2d1690521b34d4564f5ff960bcb
mariomthree <mario99manuel@gmail.com>
2022-09-13 13:19:56 +0200

    send mobile data

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ImageBackground } from 'react-native';
+import { styles } from './styles';
+import { backgroundImage } from '../../assets/background-galaxy.png';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+    return (
+        <ImageBackground style={styles.container} source={backgroundImage} defaultSource={backgroundImage}>
+            {children}
+        </ImageBackground>
+    );
+}
