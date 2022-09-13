029892da5b5cb056f84eb884d2903a904a828c7f
mariomthree <mario99manuel@gmail.com>
2022-09-13 10:03:00 +0200

    create web layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,76 @@
+import './styles/main.css';
+import logo from './assets/logo.svg'
+import { MagnifyingGlassPlus } from 'phosphor-react'
 
 function App() {
-  return (<h1>Ola, Mundo</h1>)
+  return (<div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+
+    <img className='w-[250px] mb-20' src={logo} alt="Logo" />
+
+    <h1 className='text-6xl text-white font-black mb-16'>
+      Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui
+    </h1>
+
+    <div className="grid grid-cols-6 gap-6 mb-6">
+      <a href="#" className='relative rounded-lg'>
+        <img src="/game-1.png" alt="Game" />
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block mb-1'>Lorem ipsum dolor</strong>
+          <span className='text-zinc-300 text-sm block'>10 anúncios</span>
+        </div>
+      </a>
+      <a href="#" className='relative rounded-lg'>
+        <img src="/game-2.png" alt="Game" />
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block mb-1'>Lorem ipsum dolor</strong>
+          <span className='text-zinc-300 text-sm block'>10 anúncios</span>
+        </div>
+      </a>
+      <a href="#" className='relative rounded-lg'>
+        <img src="/game-3.png" alt="Game" />
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block mb-1'>Lorem ipsum dolor</strong>
+          <span className='text-zinc-300 text-sm block'>10 anúncios</span>
+        </div>
+      </a>
+      <a href="#" className='relative rounded-lg'>
+        <img src="/game-4.png" alt="Game" />
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block mb-1'>Lorem ipsum dolor</strong>
+          <span className='text-zinc-300 text-sm block'>10 anúncios</span>
+        </div>
+      </a>
+      <a href="#" className='relative rounded-lg'>
+        <img src="/game-5.png" alt="Game" />
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block mb-1'>Lorem ipsum dolor</strong>
+          <span className='text-zinc-300 text-sm block'>10 anúncios</span>
+        </div>
+      </a>
+      <a href="#" className='relative rounded-lg'>
+        <img src="/game-6.png" alt="Game" />
+        <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+          <strong className='font-bold text-white block mb-1'>Lorem ipsum dolor</strong>
+          <span className='text-zinc-300 text-sm block'>10 anúncios</span>
+        </div>
+      </a>
+    </div>
+
+
+    <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+      <div className='bg-[#2A2634] px-8 py-6 flex justify-between'>
+        <div>
+          <strong className='text-2xl text-white font-black block'>Nao encontrou seu duo?</strong>
+          <span className='text-zinc-400 block'>Publique um anuncio para encontrar novos players</span>
+        </div>
+        <button className='py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white flex items-center gap-3'>
+          <MagnifyingGlassPlus size={24} />
+          Publicar anuncio
+        </button>
+      </div>
+    </div>
+
+  </div>);
 }
 
 export default App
