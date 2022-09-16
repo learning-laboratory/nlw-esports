ca667455fb7d71c6f799885620bde96477e774ed
mariomthree <mario99manuel@gmail.com>
2022-09-16 09:23:50 +0200

    create ad in web app

--- web/src/components/GameAdBanner.tsx
+++ web/src/components/GameAdBanner.tsx
@@ -6,7 +6,7 @@ interface GameBannerProps {
 
 export function GameAdBanner({ bannerUrl, title, adsCount }: GameBannerProps) {
     return (
-        <a href="#" className='relative rounded-lg'>
+        <a href="#" className='relative rounded-lg flex'>
             <img src={bannerUrl} alt="Game" />
             <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
                 <strong className='font-bold text-white block mb-1'>{title}</strong>
